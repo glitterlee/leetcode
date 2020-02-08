70d76f0897684111b3b0be5b557e3c0e359368e1
Teng <litengshjd@hotmail.com>
2020-02-08 02:51:54 -0800

    first commit

---
+++ src/main/java/com/teng/entities/LeetCode.java
@@ -0,0 +1,13 @@
+package com.teng.entities;
+
+public class LeetCode {
+    private String name = "Teng";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,10 @@
+<%@ page import="com.teng.entities.LeetCode" %>
+<html>
+<body>
+<h2>Hello World!</h2>
+
+<body>
+<%=new LeetCode().getName()%>
+
+</body>
+</html>
